0582c220e0f75d62581cf4b2f3eca81df2de4ff7
Kaveri <chinnamkaveri74@gmail.com>
2026-05-12 15:23:20 +0530

    Connected deployed backend

--- utils/api.js
+++ utils/api.js
@@ -2,6 +2,6 @@
 //   ? "http://10.0.2.2:5000"
 //   : "https://hand2hand-backend.onrender.com";
 
-const BASE_URL = "https://hand2hand-backend.onrender.com";
-// const BASE_URL = "http://10.0.2.2:5000";
+const BASE_URL = "https://hand2hand-backend-wt5y.onrender.com";
+
 export default BASE_URL;
